4b0e235e11bc43867f4a9c12fa09dec70fb8e7df
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2017-11-05 18:14:01 +0100

    Add Redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import ReduxLogger from 'redux-logger';
+
 import logo from './logo.svg';
 import './App.css';
+
+const store = createStore(() => {}, {}, applyMiddleware(ReduxThunk, ReduxLogger));
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </Provider>
     );
   }
 }
